ad85ee5e22fe74f8eab49cf2d82aefef37d8deeb
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-18 21:51:04 +0600

    finally done the project

--- app.js
+++ app.js
@@ -1,47 +1,24 @@
 const pionoKeys = document.querySelectorAll('.key')
 
-const playSound = (url)=>{
-    console.log(url)
-
+const playSound = (url) => {
     new Audio(url).play()
 
 }
 
-
-
-// for(const pionoKey of pionoKeys ){
-
-
-    
-//     const newUrl = `./piono-sound-24-keys/key18.mp3`
-    
-//     pionoKey.addEventListener('click',()=>{
-//         playSound(newUrl)
-//         console.log(pionoKeys)
-//     } )
-
-// }
-
-
-
-pionoKeys.forEach((pionoKey,i)=>{
-
-    const num = i < 9 ? '0'+ (i+1) : (i+1);
-    console.log(num)
-    
+pionoKeys.forEach((pionoKey, i) => {
 
     const newUrl = `./piono-sound-24-keys/key${num}.mp3`
-    
-    pionoKey.addEventListener('click',()=>{
+
+    pionoKey.addEventListener('click', () => {
         playSound(newUrl)
-    } )
+    })
 
 })
 
 
-   
 
 
-   
 
-  
+
+
+
