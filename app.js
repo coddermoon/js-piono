3b106c3ae877796140f2c360613d61a8c54f951b
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-18 21:49:17 +0600

    done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const keys = document.querySelectorAll('.keys')
+const pionoKeys = document.querySelectorAll('.key')
 
 const playSound = (url)=>{
     console.log(url)
@@ -9,16 +9,34 @@ const playSound = (url)=>{
 
 
 
-for(const key of keys ){
+// for(const pionoKey of pionoKeys ){
+
+
     
-    const newUrl = `./piono-sound-24-keys/key18.mp3`
+//     const newUrl = `./piono-sound-24-keys/key18.mp3`
     
-    key.addEventListener('click',()=>{
+//     pionoKey.addEventListener('click',()=>{
+//         playSound(newUrl)
+//         console.log(pionoKeys)
+//     } )
+
+// }
+
+
+
+pionoKeys.forEach((pionoKey,i)=>{
+
+    const num = i < 9 ? '0'+ (i+1) : (i+1);
+    console.log(num)
+    
+
+    const newUrl = `./piono-sound-24-keys/key${num}.mp3`
+    
+    pionoKey.addEventListener('click',()=>{
         playSound(newUrl)
     } )
 
-}
-
+})
 
 
    
